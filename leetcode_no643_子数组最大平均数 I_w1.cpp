dfa65ca01dbfcc9ef0664a0b7995d9d1130bc015
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-04 13:30:22 +0800

    Add files via upload

---
+++ leetcode_no643_子数组最大平均数 I_w1.cpp
@@ -0,0 +1,80 @@
+﻿/* ------------------------------------------------------------------|
+给定 n 个整数，找出平均数最大且长度为 k 的连续子数组，并输出该最大平均数。
+
+ 
+
+示例：
+
+输入：[1,12,-5,-6,50,3], k = 4
+输出：12.75
+解释：最大平均数 (12-5-6+50)/4 = 51/4 = 12.75
+ 
+
+提示：
+
+1 <= k <= n <= 30,000。
+所给数据范围 [-10,000，10,000]。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-average-subarray-i
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：148 ms, 在所有 C++ 提交中击败了86.14%的用户
+*	内存消耗：69.9 MB, 在所有 C++ 提交中击败了81.50%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+double findMaxAverage(vector<int>& nums, int k) {
+    int sum = 0;
+    for (int i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    int tmp = sum;
+    for (int i = 0, j = k; j < nums.size(); i++, j++) {
+        tmp = tmp + nums[j] - nums[i];
+        sum = max(sum, tmp);
+    }
+    return static_cast<double>(sum) / k;
+}
